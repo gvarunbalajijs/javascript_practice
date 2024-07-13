7d549f9f200bc5ac2420c8adfeb12a4e25736d5d
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-13 12:44:02 +0000

    converting

---
+++ 1_basics/3_conversion.js
@@ -0,0 +1,42 @@
+// let score=33//1 condition;
+// console.log(typeof score);
+// let score = "33"//2 condition;
+// console.log(typeof score);
+// let score="33"//3 condition;
+// let number=Number(score)
+// console.log(typeof number);
+// let score="33abcd"//condition 4;
+// let number=Number(score)
+// console.log(typeof number)
+// console.log(number)
+// let scope=null
+// let digit=Number(scope)
+// console.log(typeof digit);
+// console.log(scope);
+// console.log(digit);
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// let str1 = "hello"
+// let str2 = " varun"
+
+// let str3 = str1 + str2
+// console.log(str3);
+// let str1 = "hello"
+// let str2 = " varun"
+// console.log(str1+str2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(typeof ("1" + 2));
+// console.log(+true);
+// console.log(+"");
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
