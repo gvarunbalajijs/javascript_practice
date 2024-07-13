6923e00caf3078dfe99031424727df7e391cc57c
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-13 12:15:33 +0000

    practice

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+accountEmail="hbfg@gmail.com"
+accountPassword="13i8u"
+accountCity="Bengal"
+let accountState;
+
+//  accountId = 2 
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert(3+3)// only applicable on browser console;
+console.log(3+3)
+console.log("varun");
+let name="varun"
+let age=20
+let LoginUser=true
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof age);
+console.log(typeof LoginUser);
