28cf38d483ac79c71dac36b785ad49bd356d6427
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-14 10:50:24 +0000

    strings

---
+++ 1_basics/5_string.js
@@ -0,0 +1,14 @@
+// const animal="Bull"
+// const fight=5
+// console.log(`it is use for entertainment sport ${animal} fight and it fights ${fight} times`);//string interpolation which is also know as modern concatenation
+// const name=new String("hero")//new is a keyword and string is an object 
+// console.log(name);
+// console.log(name.__proto__);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(1));
+// console.log(name.indexOf('r'));
+const name1 = '  hero  '
+console.log(name1.trim());//trim removes the spaces storage
+const url="https:zerotohero.com//superpower"
+console.log(url.replace("superpower","superpowerful"));
