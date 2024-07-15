8591d79b2ce3bfe7954378052e9f157a403a0c16
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-15 14:01:18 +0000

    array one

---
+++ 2_basics/1_array.js
@@ -0,0 +1,31 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)//add the element in first index 
+// myArr.shift()//delete the first element in the array
+// console.log(myArr.includes(9));//tells whether the value exists in the goven array or not
+// console.log(myArr.indexOf(3));//it tells the element index
+
+// const newArr = myArr.join()//it converts the array into a string and also combine
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)//ins lice start to before end number print and in the array their is not change
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)//in splice 1 to 3 are eliminated from the array and returns the eliminated number
+// console.log("C ", myArr);
+// console.log(myn2);
