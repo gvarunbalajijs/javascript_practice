e10b635e64ee38c7ed1b3f18b5627a4848f71601
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-20 10:15:32 +0000

    switch statement

---
+++ 4_controlflow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) { //syntax
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default value");
+        break;
+}//output march, if it found the key it print all the values from where the key matched until it break and except default value
