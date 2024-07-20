8510ef932bf298b67e6093ef521dfb3ceaa95bb8
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-20 10:01:59 +0000

    if,nestedif and operators

---
+++ 4_controlflow/one.js
@@ -0,0 +1,48 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");//impicit scope is a single line we have to write we can write mutiple lines using comas and at the end we have to end using ;
+// if (balance < 500) {
+//         console.log("less than 500");
+//     } else if (balance < 750) {
+//         console.log("less than 750");
+        
+//     } else if (balance < 900) {
+//         console.log("less than 750");
+        
+//     } else {
+//         console.log("less than 1200");
+    
+//     }//print less than 1200
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2!==3) { //&& both conditions have to be true
+//     console.log("Allow to buy course");
+// }//print Allow to buy course
+
+// if (loggedInFromGoogle || loggedInFromEmail) {//multiple conditions one of the condition have to be true
+//     console.log("User logged in");
+// } // print User logged in
