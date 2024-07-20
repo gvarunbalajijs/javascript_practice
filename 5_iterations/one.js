af0d2c9c3d9f1b464fdc0b51c5d5e9b4fd461736
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-20 12:13:49 +0000

    for , break , continue

---
+++ 5_iterations/one.js
@@ -0,0 +1,83 @@
+// let a=[4,5,6,7]
+// // for (let i = 0; i < a.length; i++) {
+// //     const element = a[i];
+// //     console.log(element);
+// // }
+// console.log(a.length);
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+for (let i = 1; i <= 20; i++) {
+    // console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+// let myarray = ['trim','frim','slight','fight']
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }//print trim
+// frim
+// slight
+// fight
+// let myarray = ['trim','frim','slight','fight']
+// console.log(`length of the array:`,myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+    
+//     const element = myarray[index];
+    
+//     console.log(element);
+//     console.log(`length of the element ${element}:`,element.length);
+// }//print the array element length like
+// length of the array: 4
+// trim
+// length of the element trim: 4
+// frim
+// length of the element frim: 4
+// slight
+// length of the element slight: 6
+// fight
+// length of the element fight: 5
+
+// for (let index = 0; index <= 10; index++) {
+//     if (index==5) {
+//         console.log(`5 is detected`);
+//         break;
+//     }
+//     console.log(`value of the index ${index}`);
+//     continue;
+    
+// // }print value of the index 1
+// // value of the index 2
+// // value of the index 3
+// // value of the index 4
+// // 5 is detected
+
+//continue ex
+// for (let index = 0; index <= 10; index++) {
+//     if (index==5) {
+//         console.log(`5 is detected `);
+//         continue;
+//     }
+//     console.log(`value of the index ${index}`);
+//     continue;
+    
+// // }print value of the index 0
+// // value of the index 1
+// // value of the index 2
+// // value of the index 3
+// // value of the index 4
+// // 5 is detected 
+// // value of the index 6
+// // value of the index 7
+// // value of the index 8
+// // value of the index 9
+// // value of the index 10
