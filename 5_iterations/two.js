7a52b2b317da1f40586a54c00206c19eecec6ca1
gvarunbalajijs <gvarunbalaji9@gmail.com>
2024-07-20 12:37:44 +0000

    while and do while loop

---
+++ 5_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+// let index=0;
+// while (index<=10) {
+//     console.log(`value of index${index}`);
+//     index = index+2;
+// }
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+//  }//print Value is flash
+// // Value is batman
+// // Value is superman
+
+
+// let score = 1
+
+// do { //do is like if condition
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);//while is like a beak point
